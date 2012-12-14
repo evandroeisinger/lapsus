182f2fc7accc867f2fd312ebbb80649e44e1845f
Evandro Eisinger <evandroeisinger@gmail.com>
2012-12-14 16:50:04 -0200

    Initial structure

---
+++ js/lapsus.js
@@ -0,0 +1,14 @@
+/*!
+ * Lapsus JavaScript Application v1.0.0
+ * http://evandroeisinger.com/lapsus
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2012, Evandro Eisinger
+ * http://evandroeisinger.com
+ */
+
+window.Lapsus = (function() {
+
+}());
